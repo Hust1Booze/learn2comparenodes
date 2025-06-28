a881b1c5a11f61b74ea1fa0a681e86d67892e521
Hust1Booze <2190288283@qq.com>
2025-06-28 14:21:54 +0800

    修改脚本

--- scrips/dt_train_A100.sh
+++ scrips/dt_train_A100.sh
@@ -7,6 +7,7 @@
 #SBATCH --gres=gpu:4
 #SBATCH --qos=a100
 
+nvidia-smi
 # module load cuda/11.8
 source activate bnb
 
@@ -20,7 +21,10 @@ mkdir -p checkpoints
 # 设置环境变量以解决GCC版本兼容性问题
 export TORCH_CUDA_ARCH_LIST="8.0"
 export CUDA_HOME=/lab/cuda/cuda-11.8
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export NVCC_APPEND_FLAGS="-allow-unsupported-compiler"
 
+# python node_selection/behaviour_gen.py -n_cpu 20
+
 # 使用DeepSpeed启动多GPU训练，明确指定GPU设备
-deepspeed learning/dt_train.py 
+deepspeed learning/dt_train_ds.py  
